53275ae8cc92d3e7f8a1040056fbf83d3b9ae197
Nicolene Marais <nici.marais.ie@gmail.com>
2023-01-18 10:19:35 +0000

    trying to figure out why css file not working

--- blog/admin.py
+++ blog/admin.py
@@ -8,18 +8,17 @@ class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
-    search_fields = ('name', 'email', 'created_on')
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    actions = ['approved_comments']
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
-    def approved_comments(self, request, queryset):
+    def approve_comments(self, request, queryset):
         queryset.update(approved=True)
